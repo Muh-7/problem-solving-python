36b702d3727d8a1e35a86bc79be8dbe34a921109
mohamad <mohamad@gmail.com>
2026-01-25 22:23:20 +0300

    Adding 07_StairCase.py

--- Warmup/05_diagonal_difference.py
+++ Warmup/05_diagonal_difference.py
@@ -1,4 +1,4 @@
-# 5
+#5
 # Diagonal Difference
 
 # Way 1
---
+++ Warmup/07_StairCase.py
@@ -0,0 +1,14 @@
+#7
+# StairCase
+
+def staircase(n):
+    for i in range(n):
+        print(' ' * (n - 1 - i) + '#' * (i + 1))
+
+
+staircase(4)
+
+# Explanation:
+# For each row i (0 to n-1):
+# - spaces = n-1-i
+# - hashes = i+1
