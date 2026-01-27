c4475f5f0c4fe121b49d4d6cedc9c14a4e1af3d6
mohamad <mohamad@gmail.com>
2026-01-27 12:10:33 +0300

    Editing 09

--- Warmup/09_Birthday_Cake_Candles.py
+++ Warmup/09_Birthday_Cake_Candles.py
@@ -1,6 +1,8 @@
 #9
 # Birthday Cake Candles
 
+from collections import Counter #in way4
+
 #Way1
 def birthdayCakeCandles(candles):
     highest = max(candles)
@@ -13,7 +15,7 @@ def birthdayCakeCandles_2(candles):
 
 #Way3
 def birthdayCakeCandles_3(candles):
-    highest = 0
+    highest = candles[0]
     counter = 0
     
     for i in candles:
@@ -24,8 +26,30 @@ def birthdayCakeCandles_3(candles):
             counter +=1
     return counter
 
+#Way4
+def birthdayCakeCandles_4(candles):
+    freq = Counter(candles)
+    return freq[max(freq)]
+
+#Way5
+def birthdayCakeCandles_5(candles):
+    highest = candles[0]
+    count = 0
+    for c in candles:
+        if c >= highest:
+            count = count + 1 if c == highest else 1
+            highest = c
+    return count
+
+#Way6 (Nice)
+def birthdayCakeCandles_6(candles):
+    m = max(candles)
+    return sum(1 for c in candles if c == m)
 
 #test
 print(birthdayCakeCandles([1, 3, 5, 5, 5,2]))    
 print(birthdayCakeCandles_2([1, 3, 5, 5, 5,2]))
 print(birthdayCakeCandles_3([1, 3, 5, 5, 5,2]))
+print(birthdayCakeCandles_4([1, 3, 5, 5, 5,2]))
+print(birthdayCakeCandles_5([1, 3, 5, 5, 5,2]))
+print(birthdayCakeCandles_6([1, 3, 5, 5, 5,2]))
