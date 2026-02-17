77f2a1cb8024d23409766e7d9d2308036114e415
mohamad <mohamad@gmail.com>
2026-02-17 21:06:02 +0300

    Adding 04_between_tow_sets

---
+++ Implementaion/04_between_tow_sets.py
@@ -0,0 +1,22 @@
+#4
+# Between two sets: Find the number of integers that are between two sets.
+
+# Way1
+def getTotalX(a, b):
+    count = 0
+    for i in range(max(a), min(b)+1):
+        if all(i % j == 0 for j in a) and all(j % i == 0 for j in b):
+            count += 1
+    return count
+
+# Way2 
+def getTotalX(a, b):
+    count = 0
+    for i in range(max(a), min(b)+1):
+        if all(i% ai == 0 for ai in a):
+            if all(bj % i == 0 for bj in b):
+                count += 1
+    return count
+
+#test
+print(getTotalX([2, 4], [16, 32, 96]))
--- Implementaion/test.py
+++ Implementaion/test.py
@@ -1,2 +1,15 @@
 n = 3
 print(int(''.join([str(a) for a in range(1,n+1)]))) 
+
+#03
+print('#'*8)
+x1 = 0
+v1 = 3
+x2 = 4
+v2 = 2
+print(x1 + v1*1)
+print(x2 + v2*1)
+print(x1 + v1*2)
+print(x2 + v2*2)
+print(x1 + v1*3)
+print(x2 + v2*3)        
